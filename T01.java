d7fa2a866e07e450bde73b2ef21c49428a80e114
VentyolaNapitupulu <ventyolanapitupuluh@gmail.com>
2024-09-04 21:27:10 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,18 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24042 - Ventyola Rohati Napitupulu
+// 12S24042 - Ventyola Rohati Napitupulu
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
+
 
